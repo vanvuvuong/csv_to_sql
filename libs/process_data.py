95f792f6dcf7004ec8e5702b7979bfe74eeac4bf
Tran Dinh Dong <vanvuvuong3@gmail.com>
2021-09-23 15:53:07 +0700

    update for the pandas

--- libs/process_data.py
+++ libs/process_data.py
@@ -7,7 +7,7 @@ def process_data(csv_file: str, encoding: str, delimiter: str, quotechar: str):
 	_table_name = re.sub('[^a-zA-Z0-9-_]', '', _table_name)
 	try:
 		_data_frame = pd.read_csv(csv_file, sep=delimiter, quotechar=quotechar, squeeze=True, engine='c',
-			skipinitialspace=True, na_values='', keep_default_na=False, parse_dates=True,
+			skipinitialspace=True, na_values='', keep_default_na=False, parse_dates=True, escapechar = '\\',
 			encoding=encoding, warn_bad_lines="warn")
 		_data_frame.index = _data_frame.index + 1
 		return _table_name, _data_frame
