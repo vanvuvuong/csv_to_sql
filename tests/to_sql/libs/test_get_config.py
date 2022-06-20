62a7904f29f4a7765f7eaf5f10f38a436f48e69c
vanvuvuong <vanvuvuong3@gmail.com>
2022-06-20 14:03:04 +0700

    testing for pytest-benchmark

--- tests/to_sql/libs/test_get_config.py
+++ tests/to_sql/libs/test_get_config.py
@@ -1,14 +1,15 @@
 from to_sql.libs.get_config import get_config_yaml
 
-def get_config_yaml():
+def test_get_config_yaml(benchmark):
     file = 'to_sql/config/config.yaml'
     expected_config = {
         "server": "mysql",
-        "mysql_user": "user",
-        "mysql_root_password": "password",
-        "mysql_host": "localhost",
-        "mysql_local_port": "3306",
+        "sql_adapter": "pymysql",
+        "mysql_user": "root",
+        "mysql_root_password": "test",
+        "mysql_host": "database",
+        "mysql_local_port": 3306,
         "mysql_database": "to_sql"
     }
-    print(get_config_yaml(file))
+    benchmark(get_config_yaml, file)
     assert expected_config == get_config_yaml(file)
--- tests/to_sql/libs/test_process_data.py
+++ tests/to_sql/libs/test_process_data.py
@@ -1,8 +1,9 @@
 from to_sql.libs.process_data import create_table_name
 
-def test_create_table_name():
+def test_create_table_name(benchmark):
     file_name = 'to_sql/files/samples.csv'
     expected_result = 'samples', 'csv'
     result = create_table_name(file_name)
+    benchmark(create_table_name, file_name)
     assert expected_result[0] == result[0].split('_')[0]
     assert expected_result[1] == result[1]
